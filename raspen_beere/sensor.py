3204fd9f52c6c883f0348aa342380af51c2cd431
JannisStraus <jannisstraus1408@gmail.com>
2025-03-02 21:38:02 +0100

    make faster

--- raspen_beere/sensor.py
+++ raspen_beere/sensor.py
@@ -1,7 +1,3 @@
-import asyncio
-import time
-from typing import Callable
-
 import adafruit_dht
 import board
 
@@ -10,27 +6,41 @@ class DHT22:
     def __init__(self) -> None:
         self.dht_device = adafruit_dht.DHT22(board.D4)
 
-    async def get_temperature(self) -> float:
-        # Führt den blockierenden Aufruf in einem separaten Thread aus
-        return await asyncio.to_thread(self._try_get, self._get_temperature)
-
-    async def get_humidity(self) -> float:
-        return await asyncio.to_thread(self._try_get, self._get_humidity)
-
-    def _try_get(self, func: Callable[[], float]) -> float:
-        for _ in range(5):
-            try:
-                value = func()
-                return value
-            except RuntimeError:
-                time.sleep(0.5)  # blockierender Aufruf - wird im Thread ausgeführt
-                continue
-        raise TimeoutError("There is a problem with the DHT22 Sensor.")
-
-    def _get_temperature(self) -> float:
-        temperature = self.dht_device.temperature
+    async def get_temperature(self) -> float | None:
+        try:
+            temperature = self.dht_device.temperature
+        except RuntimeError:
+            return None
         return float(temperature)
 
-    def _get_humidity(self) -> float:
-        humidity = self.dht_device.humidity
-        return float(humidity)
+    async def get_humidity(self) -> float | None:
+        try:
+            humidity = self.dht_device.humidity
+            return float(humidity)
+        except RuntimeError:
+            return None
+
+    # async def get_temperature(self) -> float:
+    #     # Führt den blockierenden Aufruf in einem separaten Thread aus
+    #     return await asyncio.to_thread(self._try_get, self._get_temperature)
+
+    # async def get_humidity(self) -> float:
+    #     return await asyncio.to_thread(self._try_get, self._get_humidity)
+
+    # def _try_get(self, func: Callable[[], float]) -> float:
+    #     for _ in range(5):
+    #         try:
+    #             value = func()
+    #             return value
+    #         except RuntimeError:
+    #             time.sleep(0.5)  # blockierender Aufruf - wird im Thread ausgeführt
+    #             continue
+    #     raise TimeoutError("There is a problem with the DHT22 Sensor.")
+
+    # def _get_temperature(self) -> float:
+    #     temperature = self.dht_device.temperature
+    #     return float(temperature)
+
+    # def _get_humidity(self) -> float:
+    #     humidity = self.dht_device.humidity
+    #     return float(humidity)
